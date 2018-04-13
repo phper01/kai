2af192bf57f4eb4b542d8627324f362b055a1147
allen <qiubowei@foxmail.com>
2018-04-13 15:34:19 +0800

    第一版

---
+++ data/js/0.js
@@ -0,0 +1,11 @@
+<!--
+document.write("·<a href='/cn/tuji/2013/0831/50.html'>测试图集效果</a><br>");
+document.write("·<a href='/cn/news/CompanyNews/49.html'>测试视频发布</a><br>");
+document.write("·<a href='/cn/news/gonggao/48.html'>测试公告</a><br>");
+document.write("·<a href='/en/Product/Samsung/46.html'>ithium battery, battery l</a><br>");
+document.write("·<a href='/en/Product/Samsung/45.html'>Samsung mobile phone: 5.3</a><br>");
+document.write("·<a href='/en/Product/Iphone/44.html'>European version of the c</a><br>");
+document.write("·<a href='/en/Product/Iphone/43.html'>　Samsung i9220 Galaxy N</a><br>");
+document.write("·<a href='/en/Product/Iphone/42.html'>Samsung mobile phone: 5.3</a><br>");
+
+-->
---
+++ templets/default/js/BaiduSta.js
@@ -0,0 +1,2 @@
+var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3F1cefaf6a288c9920a37f5b20f82d0b7d' type='text/javascript'%3E%3C/script%3E"));
