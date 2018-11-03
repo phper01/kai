7eda36c09effba841a8378007575b6d7abb60db2
phper01 <qiubowei@foxmail.com>
2018-11-03 13:14:34 +0800

    s

--- index.php
+++ index.php
@@ -381,27 +381,5 @@
 
 
 </div>
-<script src="/templets/default/js/wySilder.min.js" type="text/javascript"></script>
-<script>
-    $(function (){
-        $(".js-silder").silder({
-            auto: true,//自动播放，传入任何可以转化为true的值都会自动轮播
-            speed: 20,//轮播图运动速度
-            sideCtrl: true,//是否需要侧边控制按钮
-            bottomCtrl: true,//是否需要底部控制按钮
-            defaultView: 0,//默认显示的索引
-            interval: 3000,//自动轮播的时间，以毫秒为单位，默认3000毫秒
-            activeClass: "active",//小的控制按钮激活的样式，不包括作用两边，默认active
-        });
-    });
-</script>
-<script type="text/javascript" src="/templets/default/js/TabShow.js"></script>
-<script type="text/javascript">
-    window.onerror = function () {
-        return true;
-    }
-</script>
-
-
 </body>
 </html>
